f7b27fa6ecd52aa5c9c8b39518eed74642245065
reyvan-d <reyvan-d@student.42.fr>
2017-06-08 18:32:21 +0200

    Working CMake and shared library generation

---
+++ includes/nibbler.h
@@ -0,0 +1,10 @@
+//
+// Created by Andromaleus on 2017-06-08.
+//
+
+#ifndef NIBBLER_NIBBLER_H
+#define NIBBLER_NIBBLER_H
+
+#include <iostream>
+
+#endif //NIBBLER_NIBBLER_H
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Andromaleus on 2017-06-08.
+//
+
+#include "../includes/nibbler.h"
+
+int main()
+{
+    std::cout << "CMake working!" << std::endl;
+    std::cout << "Debugger working!" << std::endl;
+    return 0;
+}
+
